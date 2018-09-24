dbc356f30d5d087ea650b197282df6ba925b04a5
tomastrajan <tomas.trajan@gmail.com>
2018-09-24 19:18:26 +0200

    chore(model): remove compiled files, adjust .gitignore

--- src/model/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { Rule } from '@angular-devkit/schematics';
-import { Schema as ModelServiceOptions } from './schema';
-export declare function modelService(options: ModelServiceOptions): Rule;
